e25bf2c2c755f8d2ed80ede3d4e66444aca68618
TalhahSamar <86531578+TalhahSamar@users.noreply.github.com>
2024-06-22 12:31:23 +0530

    Networking project

---
+++ Client-Server Communication (Socket Programming)/C Program Files/3 - THREAD/TCP_Client.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define PORT 1234
+
+// sleep(1) has been added such that we
+// can see what exactly is happening
+
+char* Int_Char(int n){
+    int m = n;
+    int a = 0;
+    while(m){
+        a++;
+        m/=10;
+    }
+    char* Array_1;
+    char Array_2[a];
+    Array_1 = (char*)malloc(a);
+    int x = 0;
+    while (n){
+        Array_2[++x] = n%10 + '0';
+        n/=10;
+    }
+	int i=0;
+    for(i = 0; i<x; i++) {
+        Array_1[i] = Array_2[x-i];
+    }
+    Array_1[i] = '\0';
+    return (char*)Array_1;
+}
+
+void* Thread_Cli(void* args){
+	struct sockaddr_in Address_1;
+	int Int_Cli = 1;
+	char* Int_Arr = Int_Char(Int_Cli);
+    pthread_exit(NULL);
+    return 0;
+}
+
+int main(){
+	pthread_t tid;
+	int Int_Cli = 1;
+    pthread_create(&tid, NULL, Thread_Cli, &Int_Cli);
+
+
+	struct sockaddr_in Address_1;
+	char Int_Arr[1024];
+	int Soc_Client = socket(AF_INET, SOCK_STREAM, 0);
+	if(Soc_Client < 0){
+		printf("***CONNECTION ERROR***\n");
+		sleep(1);
+		exit(1);
+	}
+	sleep(1);
+	printf("***SOCKET CREATED BY THE CLIENT***\n");
+	sleep(1);
+	memset(&Address_1, '\0', sizeof(Address_1));
+	Address_1.sin_family = AF_INET;
+	Address_1.sin_port = htons(PORT);
+	Address_1.sin_addr.s_addr = inet_addr("127.0.0.1");
+	int val = connect(Soc_Client, (struct sockaddr*)&Address_1, sizeof(Address_1));
+	if(val < 0){
+		printf("***CONNECTION ERROR***\n");
+		sleep(1);
+		exit(1);
+	}
+	printf("***CONNECTION ESTABLISHED WITH THE SERVER***\n");
+	sleep(1);
+	while(1){
+		printf("SEND REQUEST TO THE SERVER (ENTER INTEGER): ");
+		scanf("%s", &Int_Arr[0]);
+		send(Soc_Client, Int_Arr, strlen(Int_Arr), 0);
+		sleep(1);
+		if(strcmp(Int_Arr, "end") == 0){
+			sleep(1);
+			close(Soc_Client);
+			printf("***CONNECTION DISCONNECTED FROM THE SERVER***\n");
+			sleep(1);
+			exit(1);
+		}
+		if(recv(Soc_Client, Int_Arr, 1024, 0) < 0){
+			printf("***DATA RECEPTION ERROR***\n");
+			sleep(1);
+		}
+		else{
+			printf("ACKNOWLEDGEMENT FROM SERVER: %llu\n", atoll(Int_Arr));
+			sleep(1);
+		}
+	}
+	return 0;
+}
---
+++ Client-Server Communication (Socket Programming)/C Program Files/4 - NON BLOCKING/3 - EPOLL/TCP_Server.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/epoll.h> 
+
+#define MAX_CLIENTS 10
+#define PORT 1234
+
+// sleep(1) has been added such that we
+// can see what exactly is happening
+
+struct epoll_fd{
+    int fd;
+	short events;
+};
+
+// int epoll_create(int n){
+// 	return epoll_info_by_epoll_fd.size();
+// }
+
+
+void Save_Details(char* a, int b){
+	FILE *fptr;
+    fptr = fopen("value.txt", "a");
+    fprintf(fptr, "CLIENT  -->  IP ADDRESS: %s, & ", a);
+    fprintf(fptr, "PORT NUMBER: %d\n", b);
+    fclose(fptr);
+}
+
+// factorial function has been implemented,
+// for calculating the factorial of the number,
+// requested by the client.
+unsigned long long Fact(char a[]);
+unsigned long long Fact(char a[]){
+    int n = atoi(a);
+    unsigned long long fact = 1;
+    if(n>0){
+        for(int i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+    }
+    return fact;
+}
+
+int main(){
+	struct sockaddr_in Address_1;
+	int Soc_Server;
+	struct sockaddr_in Address_2;
+	socklen_t Size_Add;
+	char Int_Arr[1024];
+	pid_t Fork_Pro;
+	int Socket_1 = socket(AF_INET, SOCK_STREAM, 0);
+	if(Socket_1 < 0){
+		printf("***CONNECTION ERROR***\n");
+		sleep(1);
+		exit(1);
+	}
+	sleep(1);
+	printf("***SOCKET CREATED BY THE SERVER***\n");
+	sleep(1);
+	memset(&Address_1, '\0', sizeof(Address_1));
+	Address_1.sin_family = AF_INET;
+	Address_1.sin_port = htons(PORT);
+	Address_1.sin_addr.s_addr = inet_addr("127.0.0.1");
+	int val = bind(Socket_1, (struct sockaddr*)&Address_1, sizeof(Address_1));
+
+	int f_d = epoll_create(1);
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = Socket_1;
+    epoll_ctl(f_d, EPOLL_CTL_ADD, Socket_1, &event);
+
+	if(val < 0){
+		printf("***BINDING ERROR***\n");
+		sleep(1);
+		exit(1);
+	}
+	printf("***BINDING TO THE PORT NUMBER: %d***\n", 1234);
+	sleep(1);
+	if(listen(Socket_1, 10) == 0){
+		printf("***LISTENING.....***\n");
+		sleep(1);
+	}
+	else{
+		printf("***BINDING ERROR***\n");
+		sleep(1);
+	}
+	while(1){
+		Soc_Server = accept(Socket_1, (struct sockaddr*)&Address_2, &Size_Add);
+		if(Soc_Server < 0) exit(1);
+		printf("CONNECTION ACCEPTED FROM CLIENT <IP ADDRESS: %s> & <PORT NUMBER: %d>\n", inet_ntoa(Address_2.sin_addr), ntohs(Address_2.sin_port));
+		Save_Details(inet_ntoa(Address_2.sin_addr), ntohs(Address_2.sin_port));
+		sleep(1);
+		if((Fork_Pro = fork()) == 0){
+			close(Socket_1);
+			while(1){
+				recv(Soc_Server, Int_Arr, 1024, 0);
+				sleep(1);
+				if(strcmp(Int_Arr, "end") == 0){
+					printf("DISCONNECTED FROM CLIENT <IP ADDRESS: %s> & <PORT NUMBER: %d>\n", inet_ntoa(Address_2.sin_addr), ntohs(Address_2.sin_port));
+					sleep(1);
+					break;
+				}
+				else{
+					printf("CLIENT [IP ADDRESS: %s, PORT NUMBER: %d] ----> %llu\n", inet_ntoa(Address_2.sin_addr), ntohs(Address_2.sin_port), Fact(Int_Arr));
+					sleep(1);
+					sprintf(Int_Arr, "%llu", Fact(Int_Arr));
+					send(Soc_Server, Int_Arr, strlen(Int_Arr), 0);
+					sleep(1);
+					bzero(Int_Arr, sizeof(Int_Arr));
+				}
+			}
+		}
+	}
+	close(Soc_Server);
+	return 0;
+}
